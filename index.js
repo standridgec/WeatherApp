1adfa948b572abb2f565660e0b5b74f4f60ed19c
standridgec <143578701+standridgec@users.noreply.github.com>
2023-09-04 15:12:24 -0700

    added files

---
+++ index.js
@@ -0,0 +1,73 @@
+function formatDate(date) {
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[date.getDay()];
+
+  return `${day} ${hour}:${minute}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+  searchCity(searchInput.value);
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#searchCity").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "002c4bc7cb59f6c6e90556b79a1f828a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function searchLocation(position) {
+  let apiKey = "67757ed14562acdbed0802799f75b66e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let timeElement = document.querySelector(".searchTime");
+let currentTime = new Date();
+timeElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", search);
+
+let currentButton = document.querySelector("#currentlocation");
+currentButton.addEventListener("click", getCurrentLocation);
